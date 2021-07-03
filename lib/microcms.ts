0a32b2d22ef1930502c48e094300e9f0aa515b94
tiwuofficial <theimaginationwasunbroken@gmail.com>
2021-07-03 15:19:29 +0900

    use microcms-sdk

---
+++ lib/microcms.ts
@@ -0,0 +1,22 @@
+import { createClient } from 'microcms-js-sdk';
+
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_ENDPOINT ?? '',
+  apiKey: process.env.MICROCMS_API_KEY ?? '',
+});
+
+export type Work = {
+  id: string,
+  title: string,
+}
+
+type WorksAPIResponse = {
+  contents: Work[]
+}
+
+export const getWorkList = async (): Promise<Work[]> => {
+  const response = await client.get<WorksAPIResponse>({
+    endpoint: 'works',
+  });
+  return response.contents;
+};
